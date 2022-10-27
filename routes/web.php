e3a1b8c3fd015a0f81231979f003111bac7c4cc2
adeepak <adeepak@oritsl.com>
2022-10-27 09:01:10 +0530

    done

--- routes/web.php
+++ routes/web.php
@@ -31,3 +31,6 @@ Route:: get("/",[ProductController::class,'index']);
 Route:: get("detail/{id}",[ProductController::class,'detail']);
 Route:: get("search",[ProductController::class,'search']);
 Route:: post("add_to_cart",[ProductController::class,'addToCart']);
+Route:: get("cartList",[ProductController::class,'cartList']);
+Route:: get("removecart/{id}",[ProductController::class,'removeCart']);
+
